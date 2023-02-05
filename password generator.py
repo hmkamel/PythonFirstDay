e2e26c4ddc0514b34dcb8803a0f93f7909448ee6
unknown <mkamel.ha@gmail.com>
2023-02-05 18:20:51 -0500

    deleted some branches

--- password generator.py
+++ password generator.py
@@ -8,7 +8,7 @@ numNum = int(input("How many numbers would you like?"))
 
 def get_random_letter(length):
     letters = string.ascii_letters
-    result_letters = ''.join(rand.choice(letters)for i in range(length))
+    result_letters = ''.join(rand.choice(letters) for i in range(length))
     return result_letters
     print(result_letters)
 
@@ -28,13 +28,12 @@ def get_random_symbol(length):
     print(result_symb)
 
 
-
 finalLetter = get_random_letter(letterNum)
 finalNumber = get_random_number(numNum)
 finalSymbol = get_random_symbol(symbNum)
 
-total = finalLetter.__str__() + finalNumber.__str__()  + finalSymbol.__str__()
+total = finalLetter.__str__() + finalNumber.__str__() + finalSymbol.__str__()
 
 finalPassword = list(total)
 rand.shuffle(finalPassword)
-print("Here is your password: " ,''.join(finalPassword))
+print("Here is your password: ", ''.join(finalPassword))
